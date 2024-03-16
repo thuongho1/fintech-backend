19eda52ff96d4a70d9476b7a32dce7c3e69f2da4
Thuong Ho <thuong.ho@theiconic.com.au>
2024-03-16 13:26:29 +0700

    db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,13 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+@Schema()
+export class User {
+  @Prop({ required: true })
+  name: string;
+  @Prop()
+  age: number;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
